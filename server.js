4ac4849876d1db37b561948c0d311f6e0102a868
peeb999 <peter.bishay01@gmail.com>
2018-09-11 15:55:48 +1000

    added crud functions for subjects

--- server.js
+++ server.js
@@ -20,11 +20,7 @@ const sequelize = new Sequelize('postgres', 'postgres', 'Sugmaseng', {
   });
 
 const Users = sequelize.import('./models/users');
-
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
-}
+const Subjects = sequelize.import('./models/subjects');
 
 var server = restify.createServer();
 
@@ -36,7 +32,6 @@ server.use(restify.plugins.bodyParser({
 }));
 server.use(restify.plugins.acceptParser(server.acceptable));
 
-server.get('/hello/:name', respond);
 server.post('/login', function(req, res, next){
   const username = req.body.username
   const password = req.body.password
@@ -49,9 +44,14 @@ server.post('/login', function(req, res, next){
   }).then(user => {
     if (user.password == password) {
       res.send(200, user.id);
+      return next();
+    } else {
+      res.send(400);
+      return next();
     }
+  }).catch(error => {
     res.send(400);
-
+    return next();
   });
 
 
@@ -64,22 +64,79 @@ server.post('/createAccount', function(req, res, next){
   const email    = req.body.email
 
   Users.create({username: username, password: password, email: email}).then(user => {
-    res.send(200, user.id);
+    res.send(201, user.id);
     return next();
   }).catch(error => {
-    return res.send(400);
-  })
-
-  //
-  
+    res.send(400);
+    return next();
+  });
 
 });
 
-server.get('/subjects/:userid', function(req, res, next){
+server.get('/subjects/:user_id', function(req, res, next){
+  const user_id = req.params.user_id;
+  Subjects.findAll({
+    where: {
+      user_id: user_id
+    }
+  }).then(subjects => {
+    res.send(200, subjects);
+    return next();
+  }).catch(error => {
+    res.send(400, error);
+    return next();
+  });
+});
 
+server.post('/subjects', function(req, res, next){
+  const user_id = req.body.user_id;
+  const name = req.body.name;
+  const goal_mark = req.body.goal_mark;
 
-})
-server.head('/hello/:name', respond);
+  Subjects.create({user_id: user_id, name: name, goal_mark: goal_mark}).then(subject => {
+    res.send(201, subject.id);
+    return next();
+  }).catch(error => {
+    res.send(400, error);
+    return next();
+  })
+});
+
+server.put('/subjects', function(req, res, next){
+  const id = req.body.id;
+  //const user_id = req.body.user_id;
+  const name = req.body.name;
+  const goal_mark = req.body.goal_mark;
+
+  Subjects.update({
+    name: name,
+    goal_mark: goal_mark
+  },{
+    where: { id: id }
+  }).then(subject => {
+    res.send(200, subject);
+    return next();
+  }).catch(error => {
+    res.send(400, error);
+    return next();
+  })
+});
+
+server.del('/subjects/:id', function(req, res, next){
+  const id = req.params.id;
+
+  Subjects.destroy({
+    where: {
+      id: id
+    }
+  }).then(() => {
+    res.send(200);
+    return next();
+  }).catch(() => {
+    res.send(200);
+    return next();
+  })
+});
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
