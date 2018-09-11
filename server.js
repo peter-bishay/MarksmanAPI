1345f026dab770e998f2ce1ca2afd0128f3c5700
Alvin Soh <alvin.soh51@hotmail.com>
2018-09-11 13:31:38 +1000

    fix restify server

--- server.js
+++ server.js
@@ -22,11 +22,30 @@ function respond(req, res, next) {
 }
 
 var server = restify.createServer();
+
+server.use(restify.plugins.queryParser({
+  mapParams: true
+}));
+server.use(restify.plugins.bodyParser({
+  mapParams: true
+}));
+server.use(restify.plugins.acceptParser(server.acceptable));
+
 server.get('/hello/:name', respond);
 server.post('/login', function(req, res, next){
 
 
-    res.send(200 );
+    res.send(400);
+
+});
+
+
+server.post('/createAccount', function(req, res, next){
+  const username = req.body.username
+  const password = req.body.password
+  const email    = req.body.email
+
+  res.send(200, username);
 
 });
 
