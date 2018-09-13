9f691293f364516dccfbcaacca42f35b27727044
peeb999 <peter.bishay01@gmail.com>
2018-09-13 16:14:00 +1000

    Made some changes, but alvin is doing it

--- create_table.sql
+++ create_table.sql
@@ -7,14 +7,14 @@ CREATE TABLE users (
 
 CREATE TABLE subjects (
     id SERIAL PRIMARY KEY,
-    user_id int REFERENCES users (id),
+    user_id int REFERENCES users (id) ON DELETE CASCADE,
     name varchar(255) NOT NULL,
     goal_mark int NOT NULL
 );
 
 CREATE TABLE assessments (
 	id SERIAL PRIMARY KEY,
-	subject_id int REFERENCES subjects (id),
+	subject_id int REFERENCES subjects (id) ON DELETE CASCADE,
 	name varchar(255) NOT NULL,
 	total_mark int NOT NULL,
 	actual_mark int NOT NULL,
--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const sequelize = new Sequelize('postgres', 'postgres', 'Sugmaseng', {
 
 const Users = sequelize.import('./models/users');
 const Subjects = sequelize.import('./models/subjects');
+const Assesments = sequelize.import('./models/assessments');
 
 var server = restify.createServer();
 
@@ -32,6 +33,8 @@ server.use(restify.plugins.bodyParser({
 }));
 server.use(restify.plugins.acceptParser(server.acceptable));
 
+
+/*************************** User Create and login ***************************/
 server.post('/login', function(req, res, next){
   const username = req.body.username
   const password = req.body.password
@@ -73,6 +76,8 @@ server.post('/createAccount', function(req, res, next){
 
 });
 
+
+/*************************** Subject CRUDS ***************************/
 server.get('/subjects/:user_id', function(req, res, next){
   const user_id = req.params.user_id;
   Subjects.findAll({
@@ -104,7 +109,6 @@ server.post('/subjects', function(req, res, next){
 
 server.put('/subjects', function(req, res, next){
   const id = req.body.id;
-  //const user_id = req.body.user_id;
   const name = req.body.name;
   const goal_mark = req.body.goal_mark;
 
@@ -137,6 +141,29 @@ server.del('/subjects/:id', function(req, res, next){
     return next();
   })
 });
+
+/*************************** Assesment CRUD ***************************/
+server.post('/assessment', function(req, res, next){
+  const subject_id = req.body.subject_id;
+  const name = req.body.name;
+  const total_mark = req.body.total_mark;
+  const actual_mark = req.body.actual_mark;
+  const goal_mark = req.body.goal_mark;
+  const weight = req.body.weight;
+
+  Assesments.create({subject_id, name, total_mark, actual_mark, goal_mark, weight})
+    .then(assessment => {
+      res.send(201, assessment.id);
+      return next();
+    }).catch(error => {
+      res.send(400);
+      return next();
+    });
+})
+
+
+
+
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
